9b141f5696bc880c797d4fc1a189b8637a54848a
Sanjay Menaria <sanjaymenaria03@gmail.com>
2023-07-02 18:30:17 +0530

    second

--- lib/verify_otp.dart
+++ lib/verify_otp.dart
@@ -201,19 +201,6 @@ class _OtpBoxState extends State<OtpBox> {
   }
 }
 
-class NextPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Next Page'),
-      ),
-      body: Center(
-        child: Text('Welcome to the next page!'),
-      ),
-    );
-  }
-}
 
 void main() {
   runApp(MaterialApp(
